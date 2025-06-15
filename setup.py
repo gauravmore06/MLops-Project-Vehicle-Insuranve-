2ec2c8debbdee4a3cb751c60e214894f796a1883
Gaurav <gauravmore0206@gmail.com>
2025-06-15 23:08:09 +0530

    project structure ready venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Gaurav More",
+    author_email="gauravmore0206@gmail.com",
+    packages=find_packages()
+)
